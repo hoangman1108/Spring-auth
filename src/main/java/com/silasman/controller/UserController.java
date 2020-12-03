1ccc57adfd623b95c9c9e724766456650c77c0bd
hoang man <hoangman772@gmail.com>
2020-12-03 15:39:53 +0700

    setup success but idn y fail

--- src/main/java/com/silasman/controller/UserController.java
+++ src/main/java/com/silasman/controller/UserController.java
@@ -1,45 +1,45 @@
-package com.silasman.controller;
-
-import com.silasman.model.User;
-import com.silasman.repositoty.UserRepository;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1")
-public class UserController {
-    @Autowired
-    private UserRepository userRepository;
-
-    @RequestMapping(value="/users", method = RequestMethod.GET)
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
-    }
-
-    @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public User createUser(@RequestBody User user){
-        user.set_id(ObjectId.get());
-        userRepository.save(user);
-        return userRepository.save(user);
-    }
-    
-    @RequestMapping(value = "users/{id}", method = RequestMethod.GET)
-    public User getInfo(@PathVariable("id") ObjectId id){
-        return userRepository.findBy_id(id);
-    }
-
-    @RequestMapping(value = "users/{id}", method = RequestMethod.DELETE)
-    public void deleteUser(@PathVariable("id") ObjectId id){
-        userRepository.delete(userRepository.findBy_id(id));
-    }
-
-    @RequestMapping(value = "users/{id}", method = RequestMethod.PUT)
-        public User updateUser(@PathVariable("id") ObjectId id, @RequestBody User user){
-
-        user.set_id(id);
-        return userRepository.save(user);
-    }
-}
+//package com.silasman.controller;
+//
+//import com.silasman.model.User;
+//import com.silasman.repositoty.UserRepository;
+//import org.bson.types.ObjectId;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/v1")
+//public class UserController {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @RequestMapping(value="/users", method = RequestMethod.GET)
+//    public List<User> getAllUsers(){
+//        return userRepository.findAll();
+//    }
+//
+//    @RequestMapping(value = "/users", method = RequestMethod.POST)
+//    public User createUser(@RequestBody User user){
+//        user.setId(ObjectId.get());
+//        userRepository.save(user);
+//        return userRepository.save(user);
+//    }
+//
+//    @RequestMapping(value = "users/{id}", method = RequestMethod.GET)
+//    public User getInfo(@PathVariable("id") ObjectId id){
+//        return userRepository.findBy_id(id);
+//    }
+//
+//    @RequestMapping(value = "users/{id}", method = RequestMethod.DELETE)
+//    public void deleteUser(@PathVariable("id") ObjectId id){
+//        userRepository.delete(userRepository.findBy_id(id));
+//    }
+//
+//    @RequestMapping(value = "users/{id}", method = RequestMethod.PUT)
+//        public User updateUser(@PathVariable("id") ObjectId id, @RequestBody User user){
+//
+//        user.set_id(id);
+//        return userRepository.save(user);
+//    }
+//}
